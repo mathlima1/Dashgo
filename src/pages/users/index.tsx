01b2fa4571394c58a41cfc144d6ba81c03cdc787
mathlima1 <mathlima1@hotmail.com.br>
2022-01-09 16:51:24 -0300

    Construindo a base da páginação

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,21 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Checkbox,
+    Flex,
+    Heading,
+    Icon,
+    Table,
+    Tbody,
+    Td,
+    Text,
+    Th,
+    Thead,
+    Tr
+} from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
@@ -114,8 +129,185 @@ export default function UserList() {
                                     </Button>
                                 </Td>
                             </Tr>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Matheus de Lima</Text>
+                                        <Text fontSize="sm" color="gray.300">mathelima5@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 de Abril 2021</Td>
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        fontWeight="normal"
+                                        leftIcon={<Icon
+                                            as={RiPencilLine} fontSize="16" />}
+                                    >
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Matheus de Lima</Text>
+                                        <Text fontSize="sm" color="gray.300">mathelima5@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 de Abril 2021</Td>
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        fontWeight="normal"
+                                        leftIcon={<Icon
+                                            as={RiPencilLine} fontSize="16" />}
+                                    >
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Matheus de Lima</Text>
+                                        <Text fontSize="sm" color="gray.300">mathelima5@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 de Abril 2021</Td>
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        fontWeight="normal"
+                                        leftIcon={<Icon
+                                            as={RiPencilLine} fontSize="16" />}
+                                    >
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Matheus de Lima</Text>
+                                        <Text fontSize="sm" color="gray.300">mathelima5@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 de Abril 2021</Td>
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        fontWeight="normal"
+                                        leftIcon={<Icon
+                                            as={RiPencilLine} fontSize="16" />}
+                                    >
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Matheus de Lima</Text>
+                                        <Text fontSize="sm" color="gray.300">mathelima5@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 de Abril 2021</Td>
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        fontWeight="normal"
+                                        leftIcon={<Icon
+                                            as={RiPencilLine} fontSize="16" />}
+                                    >
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Matheus de Lima</Text>
+                                        <Text fontSize="sm" color="gray.300">mathelima5@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 de Abril 2021</Td>
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        fontWeight="normal"
+                                        leftIcon={<Icon
+                                            as={RiPencilLine} fontSize="16" />}
+                                    >
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Matheus de Lima</Text>
+                                        <Text fontSize="sm" color="gray.300">mathelima5@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 de Abril 2021</Td>
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        fontWeight="normal"
+                                        leftIcon={<Icon
+                                            as={RiPencilLine} fontSize="16" />}
+                                    >
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
                         </Tbody>
                     </Table>
+
+                    <Pagination />
                 </Box>
             </Flex>
         </Box>
