66ebb5d06692ef6045dce029cb2c9b587bff9052
mathlima1 <mathlima1@hotmail.com.br>
2022-01-09 16:33:44 -0300

    Adicionando página de listagem de usuarios

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -88,7 +88,7 @@ export default function Dashboard() {
                         p="8"
                         bg="gray.800"
                         borderRadius={8}
-                    //pb="4"
+                        pb="4"
 
                     >
                         <Text fontSize="lg" mb="4">Taxa de Abertura</Text>
